06a2ed4152dc3b36d2332f1545c7528ecbaf70f3
Urs Hengartner <urs.hengartner@uwaterloo.ca>
2016-06-10 23:00:37 -0400

    fix byte order problem

--- app/src/main/java/com/PrivacyGuard/Application/Network/TransportHeader.java
+++ app/src/main/java/com/PrivacyGuard/Application/Network/TransportHeader.java
@@ -25,7 +25,7 @@ public abstract class TransportHeader extends AbsHeader {
 
     public void updateSrcPort(int srcPort) {
         this.srcPort = srcPort;
-        data[0] = (byte)(srcPort & 0xFF);
-        data[1] = (byte)(((srcPort & 0xFF)) >> 8);
+        data[1] = (byte)(srcPort & 0xFF);
+        data[0] = (byte)(((srcPort & 0xFF)) >> 8);
     }
 }
